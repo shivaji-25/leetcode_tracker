421c7d9b6929c5c39226e7a9e781e97d10b4b38c
SHIVAJI C S <shivajichandramohan97@gmail.com>
2026-08-12 11:56:49 +0530

    Create 3330-ModifyTheMatrix

---
+++ 3330-ModifyTheMatrix/3330-ModifyTheMatrix.java
@@ -0,0 +1,31 @@
+// Last updated: 12/08/2026, 11:56:49
+class Solution {
+
+    public int[][] modifiedMatrix(int[][] matrix) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[] maxCol = new int[n];
+
+        // Find maximum of each column
+        for (int j = 0; j < n; j++) {
+            maxCol[j] = Integer.MIN_VALUE;
+
+            for (int i = 0; i < m; i++) {
+                maxCol[j] = Math.max(maxCol[j], matrix[i][j]);
+            }
+        }
+
+        // Replace -1 with column maximum
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = maxCol[j];
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
