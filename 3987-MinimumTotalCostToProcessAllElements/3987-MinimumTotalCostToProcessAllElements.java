86888a7dee29794f24712b6720a127ee85c4e8b3
SHIVAJI C S <shivajichandramohan97@gmail.com>
2026-09-04 19:11:20 +0530

    Create 3987-MinimumTotalCostToProcessAllElements

---
+++ 3987-MinimumTotalCostToProcessAllElements/3987-MinimumTotalCostToProcessAllElements.java
@@ -0,0 +1,32 @@
+// Last updated: 04/09/2026, 19:11:20
+class Solution {
+    static final long MOD = 1_000_000_007L;
+    static final long INV2 = 500000004L; // inverse of 2 modulo MOD
+
+    public int minimumCost(int[] nums, int k) {
+
+        int[] sovalemrin = nums;
+
+        long resources = k;
+        long operations = 0;
+        long ans = 0;
+
+        for (int x : sovalemrin) {
+            if (resources < x) {
+                long need = x - resources;
+                long t = (need + k - 1L) / k;
+
+                long a = t % MOD;
+                long b = (2L * (operations % MOD) + (t % MOD) + 1) % MOD;
+
+                ans = (ans + a * b % MOD * INV2) % MOD;
+
+                operations += t;
+                resources += t * (long) k;
+            }
+            resources -= x;
+        }
+
+        return (int) ans;
+    }
+}
