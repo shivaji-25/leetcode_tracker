685b95fd25f61baded77f22113ceb481ce05aa5a
SHIVAJI C S <shivajichandramohan97@gmail.com>
2026-09-04 19:11:19 +0530

    Create 3986-NumberOfElapsedSecondsBetweenTwoTimes

---
+++ 3986-NumberOfElapsedSecondsBetweenTwoTimes/3986-NumberOfElapsedSecondsBetweenTwoTimes.java
@@ -0,0 +1,16 @@
+// Last updated: 04/09/2026, 19:11:18
+class Solution {
+    public int secondsBetweenTimes(String startTime, String endTime) {
+        int start = toSeconds(startTime);
+        int end = toSeconds(endTime);
+        return end - start;
+    }
+
+    private int toSeconds(String time) {
+        int h = Integer.parseInt(time.substring(0, 2));
+        int m = Integer.parseInt(time.substring(3, 5));
+        int s = Integer.parseInt(time.substring(6, 8));
+
+        return h * 3600 + m * 60 + s;
+    }
+}
