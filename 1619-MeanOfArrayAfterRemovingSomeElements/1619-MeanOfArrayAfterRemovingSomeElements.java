f87f4d61a57179d2d01470ea323344a32ae8235f
SHIVAJI C S <shivajichandramohan97@gmail.com>
2026-09-04 19:13:17 +0530

    Create 1619-MeanOfArrayAfterRemovingSomeElements

---
+++ 1619-MeanOfArrayAfterRemovingSomeElements/1619-MeanOfArrayAfterRemovingSomeElements.java
@@ -0,0 +1,12 @@
+// Last updated: 04/09/2026, 19:13:16
+class Solution {
+    public double trimMean(int[] arr) {
+        Arrays.sort(arr);
+        int remove=arr.length/20;
+        int sum=0;
+        for(int i=remove;i<arr.length-remove;i++){
+            sum+=arr[i];
+        }
+        return (double) sum/(arr.length -2 * remove);
+    }
+}
