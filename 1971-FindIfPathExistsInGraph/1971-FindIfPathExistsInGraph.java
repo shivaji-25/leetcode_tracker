b43cfabb5f52a10cabf29b9e4842e0e374289d7f
SHIVAJI C S <shivajichandramohan97@gmail.com>
2026-09-04 19:11:59 +0530

    Create 1971-FindIfPathExistsInGraph

---
+++ 1971-FindIfPathExistsInGraph/1971-FindIfPathExistsInGraph.java
@@ -0,0 +1,39 @@
+// Last updated: 04/09/2026, 19:11:59
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if (source == destination) {
+            return true;
+        }
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n];
+        queue.offer(source);
+        visited[source] = true;
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            if (current == destination) {
+                return true;
+            }
+            for (int neighbor : graph.get(current)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        return false;
+    }
+}
